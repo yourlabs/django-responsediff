a5604c1a69b9d300e02f55a6110412ecd46e4c05
jpic <jamespic@gmail.com>
2018-07-05 00:55:01 +0200

    Release 0.7.4 [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='django-responsediff',
-    version='0.7.3',
+    version='0.7.4',
     description='HTTP response diffing against fixtures for testing',
     author='James Pic',
     author_email='jamespic@gmail.com',
